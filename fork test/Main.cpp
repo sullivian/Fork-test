55f8193ea91d0a56af69686e669618395accf495
Shivansh Mathur <mathurshiva90@outlook.com>
2018-02-13 11:56:45 +0530

    Example made

---
+++ fork test/Main.cpp
@@ -0,0 +1,16 @@
+#include<Windows.h>
+
+
+int CALLBACK WinMain ( HINSTANCE Instance, HINSTANCE PrevInstance, PSTR CmdLine, int CmdShow ) {
+	HWND hwnd;
+
+	hwnd = CreateWindow ( TEXT ( "static" ), TEXT ( "static" ), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, Instance, NULL );
+	UpdateWindow ( hwnd );
+	ShowWindow ( hwnd, CmdShow );
+	MSG msg;
+	while( GetMessage ( &msg, hwnd, NULL, NULL ) ) {
+		TranslateMessage ( &msg );
+		DispatchMessage ( &msg );
+	}
+	return msg.wParam ;
+}
