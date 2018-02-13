38fb46e2c33590c224096a4a2f49d2989fdfd713
Shivansh Mathur <mathurshiva90@outlook.com>
2018-02-13 12:09:51 +0530

    a bit change

--- fork test/Main.cpp
+++ fork test/Main.cpp
@@ -4,7 +4,7 @@
 int CALLBACK WinMain ( HINSTANCE Instance, HINSTANCE PrevInstance, PSTR CmdLine, int CmdShow ) {
 	HWND hwnd;
 
-	hwnd = CreateWindow ( TEXT ( "static" ), TEXT ( "static" ), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, Instance, NULL );
+	hwnd = CreateWindow ( TEXT ( "static" ), TEXT ( "Hello Guys" ), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, Instance, NULL );
 	UpdateWindow ( hwnd );
 	ShowWindow ( hwnd, CmdShow );
 	MSG msg;
