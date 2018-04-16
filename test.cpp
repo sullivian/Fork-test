0fa5a7e7940c54d8ccc78cb72e8bf76a8e06081f
Shivansh Mathur <mathurshiva90a@gmail.com>
2018-04-16 20:14:49 +0530

    added 3rd loop

--- test.cpp
+++ test.cpp
@@ -11,7 +11,8 @@ int main()
 {
 	for (int i = 0; i < 10; ++i) {
 		for (int j = 0; j < 10; ++j) {
-			cout << "VOila!!" << i * j;
+			for (int k = 0; k < 10; ++k)
+				cout << "Voila: " << i * j * k;
 		}
 	}
 	system ( "pause" );
