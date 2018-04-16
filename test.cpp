dd421d3e8079f08930b74807b6a795bc6071b758
Shivansh Mathur <mathurshiva90@outlook.com>
2018-04-16 17:23:16 +0530

    loop added

--- test.cpp
+++ test.cpp
@@ -9,6 +9,11 @@ using namespace std;
 
 int main()
 {
+	for (int i = 0; i < 10; ++i) {
+		for (int j = 0; j < 10; ++j) {
+			cout << "VOila!!" << i * j;
+		}
+	}
 	system ( "pause" );
 	
     return 0;
