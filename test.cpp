52d9cbf00148a672655aed3d99696c487b1faa77
Shivansh Mathur <mathurshiva90@outlook.com>
2018-04-16 21:38:33 +0530

    SOmething

--- test.cpp
+++ test.cpp
@@ -9,16 +9,7 @@ using namespace std;
 
 int main()
 {
-	for (int i = 0; i < 10; ++i) {
-		for (int j = 0; j < 10; ++j) {
-			for (int k = 0; k < 10; ++k)
-				cout << "Voila: " << i * j * k; 
-			cout << "Voila: " << i * j * k;
-
-		}
-		cout << "Voila: " << i * j * k;
-
-	}
+	//Need to do SOmething
 	system ( "pause" );
 	
     return 0;
